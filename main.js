3a9757c7c333ff9e2f069e7f696483844288b158
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-12 16:01:22 -0500

    modal del post principal, creado

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('#documents').style.display = "flex";
+
+function openModal(){
+  document.querySelector('#documents').style.display = 'flex';
+}
+function closeModal(){
+  document.querySelector('#documents').style.display = 'none';
+}
