22ce35627ce7b14f30bf71374b774711aecfff27
Dayana Horna <dayisitax@gmail.com>
2022-07-13 18:09:09 -0500

    Funcionalidad JS para la geolocalización

--- main.js
+++ main.js
@@ -34,3 +34,57 @@ publish.addEventListener('click', function(){
     closeModal();
   }
 });
+
+
+calculateLocation();
+function calculateLocation() {
+  if (navigator.geolocation) {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 1000,
+    };
+    navigator.geolocation.getCurrentPosition(savePosition, showErrors, options);
+  } else {
+    alert(
+      "Tu navegador no soporta la geolocalización, actualiza tu navegador."
+    );
+  }
+}
+async function savePosition(position) {
+  let { latitude, longitude } = position.coords;
+  let response = await fetch(
+    "https://eu1.locationiq.com/v1/reverse?key=pk.d7081966f4a73ff67138855cfeb0e4ec&lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&format=json"
+  );
+  let responseJson = await response.json();
+  let { town, country } = responseJson.address;
+  let location = " " + town + ", " + country;
+  (location);
+  console.log(location);
+  document.querySelector(".geolocation_position").innerHTML = "From: " + location;
+}
+function showErrors(){
+}
+
+
+let openlocation = document.querySelectorAll(".icon-geoposition");
+let geoposition = document.querySelectorAll(".geolocation");
+
+openlocation.forEach((element, index) => {
+	element.addEventListener("click", function (e) {
+		e.preventDefault();
+		if(geoposition[index].style.display == "none"){
+			geoposition[index].style.display = "block";
+		} else {
+			geoposition[index].style.display = "none";
+		}
+	});
+});
+
+
+
+
