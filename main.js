2f843779252df738cff10cde8926fbf60b5ecae6
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-14 10:29:18 -0500

    cambios para localStorage

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-document.querySelector('#documents').style.display = "none";
-document.querySelector('.msg-error-blank').style.display = "none";
+document.querySelector("#documents").style.display = "none";
+document.querySelector(".msg-error-blank").style.display = "none";
 const error = "ERROR";
 
-let publish = document.querySelector('.publishButton');
-let openButtons = document.querySelectorAll(".public-button");
+let publish = document.querySelector(".publishButton");
+let openDropdowns = document.querySelectorAll(".public-button");
 let boxes = document.querySelectorAll(".POP");
 
 const inputImageFiles = document.querySelector(".input-file");
 const containerImages = document.querySelector(".container-images");
 let images = [];
-let post = {}
+let post = {};
 let postData = {};
 
 // Inicio de eventos para el modal de post
-function openModal(){
-  document.querySelector('#documents').style.display = 'flex';
+function openModal() {
+  document.querySelector("#documents").style.display = "flex";
 }
-function closeModal(){
-  document.querySelector('#documents').style.display = 'none';
+function closeModal() {
+  document.querySelector("#documents").style.display = "none";
 }
-function msgError(){
-  document.querySelector('.msg-error-blank').style.display = "block";
+function msgError() {
+  document.querySelector(".msg-error-blank").style.display = "block";
 }
 
-openButtons.forEach((element, index) => {
-	element.addEventListener("click", function (e) {
-		e.preventDefault();
-		if(boxes[index].style.display == "block"){
-			boxes[index].style.display = "none";
-		} else {
-			boxes[index].style.display = "block";
-		}
-	});
+openDropdowns.forEach((element, index) => {
+  element.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (boxes[index].style.display == "block") {
+      boxes[index].style.display = "none";
+    } else {
+      boxes[index].style.display = "block";
+    }
+  });
 });
 
-publish.addEventListener('click', function(event){
+publish.addEventListener("click", function (event) {
   try {
     event.preventDefault();
-    if(document.querySelector('#post-area').value == ''){
+    if (document.querySelector("#post-area").value == "") {
       msgError();
-    }else{
-      document.querySelector('.msg-error-blank').style.display = "none";
+    } else {
+      document.querySelector(".msg-error-blank").style.display = "none";
       closeModal();
     }
 
@@ -55,6 +55,7 @@ inputImageFiles.addEventListener("change", (e) => {
   images = e.currentTarget.files;
   renderImagesPreviews();
 });
+console.log('inputImageFiles', inputImageFiles);
 
 function removeImage(removedIndex) {
   images = [...images].filter((_img, index) => index != removedIndex);
@@ -79,16 +80,19 @@ function renderImagesPreviews() {
 }
 
 // Inicio de almacenamiento de datos en LocalStorage
-function saveData(){
+function saveData() {
   post = {
-    content: document.querySelector('#post-area').value
+    content: document.querySelector("#post-area").value,
     // images: images
-  }
-  localStorage.setItem('post', JSON.stringify(post));
+  };
+  localStorage.setItem("post", JSON.stringify(post));
+}
+function removeData() {
+  localStorage.removeItem("post");
 }
 
 //Inicio de mostrar los datos guardados en LocalStorage
-function showData(){
-  postData = JSON.parse(localStorage.getItem('post'));
-  document.querySelector('#text-area').value = postData.content;
+function showData() {
+  postData = JSON.parse(localStorage.getItem("post"));
+  document.querySelector("#text-area").value = postData.content;
 }
